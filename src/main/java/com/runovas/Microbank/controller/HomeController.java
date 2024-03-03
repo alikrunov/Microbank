546272090f241a2d5b1bf7d81dae22aeb8349831
alikrunov <alikrunov@gmail.com>
2024-03-03 17:04:06 +0300

    get attributes

--- src/main/java/com/runovas/Microbank/controller/HomeController.java
+++ src/main/java/com/runovas/Microbank/controller/HomeController.java
@@ -1,7 +1,15 @@
 package com.runovas.Microbank.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+import java.util.Map;
 
 @RestController
 public class HomeController {
@@ -12,7 +20,12 @@ public class HomeController {
     }
 
     @GetMapping("/secured")
-    public String secured() {
-        return "Hello, secured!";
+    public String secured(Principal principal) {
+        OAuth2AuthenticationToken oAuth2AuthenticationToken = (OAuth2AuthenticationToken)principal;
+        OAuth2User oAuth2User = oAuth2AuthenticationToken.getPrincipal();
+
+    Map<String,Object> attributes =  oAuth2User.getAttributes();
+
+        return String.format("Hello, %s!", attributes.get("login"));
     }
 }
